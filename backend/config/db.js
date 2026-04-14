5801ac83e175edaf5e3721e680a3345eabb1b6d0
shamil <ubyshamil@gmail.com>
2026-04-14 12:16:48 +0530

    before deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import pkg from "pg";
 const { Pool } = pkg;
 
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "gov_scheme_app",
+//   password: "MsdNjr71011",
+//   port: 5000
+// });
+console.log("DB URL:", process.env.DATABASE_URL);
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "gov_scheme_app",
-  password: "MsdNjr71011",
-  port: 5000
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
