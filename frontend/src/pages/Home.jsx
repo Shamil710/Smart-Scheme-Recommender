bbd4dd5215e7d75dec295f39e037e410df24d3c7
shamil <ubyshamil@gmail.com>
2026-05-07 19:19:11 +0530

    just a checking

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -115,7 +115,7 @@ export default function Home() {
               Find Government Schemes
               <br />
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-civic-300 to-amber-300">
-                You're Eligible For
+                You're Eligible Forrrrrr
               </span>
             </motion.h1>
 
